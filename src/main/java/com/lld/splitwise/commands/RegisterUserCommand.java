047044abf84d18b14c90538c69363e9dc86731e5
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-11-22 02:17:16 -0500

    implemented execute method for register user command

--- src/main/java/com/lld/splitwise/commands/RegisterUserCommand.java
+++ src/main/java/com/lld/splitwise/commands/RegisterUserCommand.java
@@ -1,5 +1,9 @@
 package com.lld.splitwise.commands;
 
+import com.lld.splitwise.controllers.UserController;
+import com.lld.splitwise.dtos.RegisterUserRequestDto;
+import com.lld.splitwise.dtos.RegisterUserResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -7,6 +11,14 @@ import java.util.List;
 
 @Service
 public class RegisterUserCommand implements Command{
+    private UserController userController;
+
+    @Autowired
+    public RegisterUserCommand(UserController userController) {
+        this.userController = userController;
+    }
+
+
     @Override
     public boolean parse(String commandLine) {
         List<String> commandTokens = Arrays.stream(commandLine.split(" ")).toList();
@@ -22,6 +34,16 @@ public class RegisterUserCommand implements Command{
 
     @Override
     public void execute(String commandLine) {
+        List<String> commandTokens = Arrays.stream(commandLine.split(" ")).toList();
+        String username = commandTokens.get(1);
+        String phoneNumber = commandTokens.get(2);
+        String password = commandTokens.get(3);
+        RegisterUserRequestDto requestDto = new RegisterUserRequestDto();
+        requestDto.setUsername(username);
+        requestDto.setPhoneNumber(phoneNumber);
+        requestDto.setPassword(password);
 
+        RegisterUserResponseDto responseDto = userController.registerUser(requestDto);
+        System.out.println(responseDto.getUser());
     }
 }
