17aa1201077b047ff42b402c057bfee8df66ca8c
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-11-22 02:19:14 -0500

    added support for populating created and last modified date fields in base model by adding annotations.

--- src/main/java/com/lld/splitwise/SplitWiseApplication.java
+++ src/main/java/com/lld/splitwise/SplitWiseApplication.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SplitWiseApplication implements CommandLineRunner {
 
     @Autowired
@@ -24,7 +26,7 @@ public class SplitWiseApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         commandRegistry.registerCommand(registerUserCommand);
         commandRegistry.registerCommand(updateProfileCommand);
 
